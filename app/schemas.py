993c39ac4e4e982487ac1b53b802a94b3513d470
Alicemerzkiy <ayusheev06.06@gmail.com>
2026-08-04 10:56:51 +0800

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,39 +1,27 @@
+from pydantic import BaseModel
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, Field
-
 class CategoryBase(BaseModel):
-    title: str = Field(..., min_length=1)
+    title: str
 
 class CategoryCreate(CategoryBase):
     pass
 
-class CategoryUpdate(BaseModel):
-    title: Optional[str] = Field(None, min_length=1)
-
 class Category(CategoryBase):
     id: int
-
-    model_config = ConfigDict(from_attributes=True)
+    class Config:
+        orm_mode = True
 
 class BookBase(BaseModel):
-    title: str = Field(..., min_length=1)
-    description: str
-    price: float = Field(..., gt=0)
-    url: str
+    title: str
+    author: str
+    year: int
     category_id: int
 
 class BookCreate(BookBase):
     pass
 
-class BookUpdate(BaseModel):
-    title: Optional[str] = Field(None, min_length=1)
-    description: Optional[str] = None
-    price: Optional[float] = Field(None, gt=0)
-    url: Optional[str] = None
-    category_id: Optional[int] = None
-
 class Book(BookBase):
     id: int
-
-    model_config = ConfigDict(from_attributes=True)
+    class Config:
+        orm_mode = True
